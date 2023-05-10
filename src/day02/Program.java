f7e381916b30df5eb81101eba6ac95890439490e
jasaswinikasturee <132183556+jasaswinikasturee@users.noreply.github.com>
2023-05-10 15:20:21 +0530

    day 02
    package, database, class

---
+++ src/day02/Program.java
@@ -0,0 +1,20 @@
+ package day02;
+
+public class Program {
+
+	//main method- line not to be executed
+	/* 
+	 paragraph not to be executed
+	 */  
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+		System.out.println("Good day..!");
+	    System.out.println("Good Morning ");
+	    System.out.println(" Hello!!");
+		
+	}
+
+	
+}
