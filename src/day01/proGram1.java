5f13b11d39582d848ebbd401ff89f93f827c8409
jasaswinikasturee <132183556+jasaswinikasturee@users.noreply.github.com>
2023-05-11 16:45:29 +0530

    day03
    database, methodwriting

--- src/day01/proGram1.java
+++ src/day01/proGram1.java
@@ -1,10 +1,33 @@
 package day01;
 
-public class proGram1 {
+public class proGram1 {  
+	static int u=30;
+	int v=30;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		proGram1 xyz= new proGram1();
+		xyz.add(101234,20);
+		mul(u,xyz.v);
+		div(1000,20);
+		xyz.sub(100,200);
+		xyz.add(10,2000);
+		}
 
+	private void add(long a, int b)
+	{
+		System.out.println(a+b);
 	}
-
+	protected void sub(int a, int b)
+	{
+		System.out.println(a-b);
+	}
+	static void mul(int a, int b)
+	{
+		System.out.println(a*b);
+	}
+	public static void div(int a, int b)
+{
+	System.out.println(a/b);
+}
 }
---
+++ src/day03/Program3.java
@@ -0,0 +1,32 @@
+package day03;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		add (101234,20);
+		mul(10,20);
+		div(1000,20);
+		sub(100,200);
+		add(10,2000);
+
+	}
+
+	private static void add(int a, int b)
+	{
+		System.out.println(a+b);
+	}
+	protected static void sub(int a, int b)
+	{
+		System.out.println(a-b);
+	}
+	 static void mul(int a, int b)
+	{
+		System.out.println(a*b);
+	}
+	public static void div(int a, int b)
+	{
+		System.out.println(a/b);
+	}
+}
