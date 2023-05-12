4536eef0b6fe88a2f0fc1ce6d41b35819f4f1430
jasaswinikasturee <132183556+jasaswinikasturee@users.noreply.github.com>
2023-05-12 08:47:07 +0530

    day04
    return value

---
+++ src/day04/Program4.java
@@ -0,0 +1,33 @@
+package day04;
+
+public class Program4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Program4 abc = new Program4();
+		int x = add(20, 40) ;
+		System.out.println(x*x);
+		System.out.println(x*x*x);
+		byte u=50;
+		byte v=30;
+		abc.sub((byte) 50, (byte)30);
+		abc.sub(u,v);
+		
+
+
+	}
+
+	
+	public static int add(int a, int b)
+	{
+		System.out.println(a+b);
+		return a+b;
+	}
+	
+	
+	public int sub(byte a, byte b)
+	{
+		return a-b;
+	}
+}
